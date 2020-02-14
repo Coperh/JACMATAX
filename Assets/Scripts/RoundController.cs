e6b391d51219ab2fc8f590f5fc4b3c5bada86c95
Conor Holden <cholden48@gmail.com>
2020-02-14 23:56:36 +0000

    Added Player Health Tracking to RoundController The round controller can now view player health infromation

--- Assets/Scripts/RoundController.cs
+++ Assets/Scripts/RoundController.cs
@@ -5,39 +5,58 @@ using UnityEngine;
 public class RoundController : MonoBehaviour
 {
 
-    [SerializeField] protected GameObject right_player; // player that spawns on right, score, health, etc. also on right
-    [SerializeField] protected GameObject left_player; // player that spawns on left, score, health, etc. also on left
+    [SerializeField] protected GameObject RightPlayerPrefab; // player that spawns on right, score, health, etc. also on right
+    [SerializeField] protected GameObject LeftPlayerPrefab; // player that spawns on left, score, health, etc. also on left
+    protected GameObject RightPlayer;
+    protected HealthSystem RightPlayerHealth;
 
+    protected GameObject LeftPlayer;
+    protected HealthSystem LeftPlayerHealth;
 
 
     // Start is called before the first frame update
     void Start()
     {
         SpawnSprites();
+        GetHealthObject();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckHealth();
+    }
+
+
+    private void CheckHealth() {
+
+        Debug.Log(RightPlayerHealth.GetHealth());
+        Debug.Log(LeftPlayerHealth.GetHealth());
+
+    }
+
+    private void GetHealthObject() {
+        RightPlayerHealth = RightPlayer.GetComponent<GenericPlayer>().health;
+        LeftPlayerHealth = LeftPlayer.GetComponent<GenericPlayer>().health;
     }
 
     private void SpawnSprites() {
 
-        Vector2 rightPos = new Vector2(10, 10);
-        Vector2 leftPos = new Vector2(20, 10);
+        Vector2 rightPos = new Vector2(20, 10);
+        Vector2 leftPos = new Vector2(10, 10);
 
 
         // create right player
-        Instantiate(
-            right_player,
+        RightPlayer = Instantiate(
+            RightPlayerPrefab,
             rightPos,
             Quaternion.identity);
 
 
         // create right player
-        Instantiate(
-            left_player,
+        LeftPlayer = Instantiate(
+            LeftPlayerPrefab,
             leftPos,
             Quaternion.identity);
     }
