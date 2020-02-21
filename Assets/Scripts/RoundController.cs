b826546bae89e63e1e9f322c88bd37118caca054
Conor Holden <cholden48@gmail.com>
2020-02-21 13:57:16 +0000

    Added death dectection When a player dies, the round will now end and go to the retry scene

--- Assets/Scripts/RoundController.cs
+++ Assets/Scripts/RoundController.cs
@@ -13,13 +13,15 @@ public class RoundController : MonoBehaviour
     protected GameObject LeftPlayer;
     protected HealthSystem LeftPlayerHealth;
 
+    private SceneLoader sceneLoader;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        GetSceneLoader();
         SpawnSprites();
         GetHealthObject();
-
         UnFreezePlayers();
     }
 
@@ -30,31 +32,54 @@ public class RoundController : MonoBehaviour
         CheckHealth();
     }
 
-    private void FreezePlayers() {
+    private void GetSceneLoader() {
+        sceneLoader = this.gameObject.GetComponent<SceneLoader>();
 
+    }
+
+    private void FreezePlayers() {
+        // freeze players 
         RightPlayer.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
         LeftPlayer.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+        // disable control scripts
         LeftPlayer.GetComponent<GenericPlayer>().enabled = false;
         RightPlayer.GetComponent<GenericPlayer>().enabled = false;
 
     }
 
     private void UnFreezePlayers() {
-        // unfreeze players
+        // unfreeze players ( keep rotation frozen)
         RightPlayer.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
         LeftPlayer.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
-
+        // enable controll scripts
         LeftPlayer.GetComponent<GenericPlayer>().enabled = true;
         RightPlayer.GetComponent<GenericPlayer>().enabled = true;
-
 
     }
 
 
+    private void EndRound(GameObject loser) {
+        FreezePlayers();
+
+        Destroy(loser);
+        // show win screen
+
+        // go onto next round
+
+        // end scene
+        sceneLoader.LoadNextScene();
+
+    }
+    
+
+
     private void CheckHealth() {
 
-        Debug.Log(RightPlayerHealth.GetHealth());
-        Debug.Log(LeftPlayerHealth.GetHealth());
+        if (RightPlayerHealth.GetHealth() <= 0) {
+            EndRound(RightPlayer);
+            }
+
+        else if (LeftPlayerHealth.GetHealth() <= 0) EndRound(RightPlayer);
 
     }
 
