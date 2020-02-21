cf335e14f50bba0bbc96d18dfdcecf58b8a853a9
Conor Holden <cholden48@gmail.com>
2020-02-21 13:24:20 +0000

    Fixed Minimum and maximum in HealthSystem Contraints were added to wrong functions Added max health variable

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HealthSystem
 {
     [SerializeField] private readonly int defaultHealth = 100;
+    [SerializeField] private readonly int maxHleath = 100;
     private int health;
 
     public HealthSystem() {
@@ -22,12 +23,12 @@ public class HealthSystem
     public void Damage(int damage)
     {
         int newHealth = health - damage;
-        if (newHealth > 100) newHealth = 100;
+        if (newHealth < 0) newHealth = 0;
         health = newHealth;
     }
     public void Heal(int healing) {
             int newHealth = health + healing;
-            if (newHealth < 0) newHealth = 0;
+            if (newHealth > maxHleath) newHealth = maxHleath;
             health = newHealth;
         }
 
